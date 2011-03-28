80bf6fade2e9b309a1f5733048ea359263d3475e
unknown <kesler@.vmware.com>
2011-03-28 15:52:43 -0400

    Remove obsolete cruft

--- elisp/Installed-packages/ESheet-0.8.4/auto-autoloads.el
+++
@@ -1,5 +0,0 @@
-(autoload 'esheet-mode "esheet.el" "makes emacs act like a spreadsheet" t)
-(autoload 'csv-mode "esheet.el" "makes emacs act like a spreadsheet" t)
-(setq auto-mode-alist (cons (cons "\\.esh\\'" 'esheet-mode) auto-mode-alist))
-(setq auto-mode-alist (cons (cons "\\.csv\\'" 'csv-mode) auto-mode-alist))
-
