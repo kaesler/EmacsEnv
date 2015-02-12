b49b17bcfc7aad369c7acda663cd711ddcd326d6
Kevin Esler <kevin.esler@gmail.com>
2015-02-12 16:29:22 -0500

    Update.

--- elpa/company-20150212.535/company-pkg.el
+++ elpa/company-20150212.535/company-pkg.el
@@ -1,4 +1,4 @@
-(define-package "company" "20150204.836" "Modular text completion framework"
+(define-package "company" "20150212.535" "Modular text completion framework"
   '((emacs "24.1")
     (cl-lib "0.5"))
   :url "http://company-mode.github.io/" :keywords
