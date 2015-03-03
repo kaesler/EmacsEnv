c18982463511629754ed23e1633be58f774269fe
Kevin Esler <kevin.esler@gmail.com>
2015-03-03 10:17:57 -0500

    Updated.

--- elpa/shm-20150205.1544/shm-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "shm" "20150205.1544" "Structured Haskell Mode" 'nil :keywords
-  '("development" "haskell" "structured"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/shm-20150226.327/shm-pkg.el
@@ -0,0 +1,5 @@
+(define-package "shm" "20150226.327" "Structured Haskell Mode" 'nil :keywords
+  '("development" "haskell" "structured"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
