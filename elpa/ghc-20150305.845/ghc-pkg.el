5a543041e73c96b57a52420fe93cbace6a022b63
Kevin Esler <kevin.esler@gmail.com>
2015-06-02 13:20:31 -0400

    Updated.

--- elpa/ghc-20150305.845/ghc-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "ghc" "20150305.845" "Sub mode for Haskell mode" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/shm-20150526.124/shm-pkg.el
@@ -0,0 +1,5 @@
+(define-package "shm" "20150526.124" "Structured Haskell Mode" 'nil :keywords
+  '("development" "haskell" "structured"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
