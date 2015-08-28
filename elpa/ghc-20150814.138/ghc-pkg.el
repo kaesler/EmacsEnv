c26d0ea2e004348e7814f752f2d8c3ddc36b39f0
Kevin Esler <kevin.esler@gmail.com>
2015-08-28 15:57:12 -0400

    Updated

--- elpa/ghc-20150814.138/ghc-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "ghc" "20150814.138" "Sub mode for Haskell mode"
-  '((haskell-mode "13.0")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/haskell-mode-20150823.2218/haskell-mode-pkg.el
@@ -0,0 +1,5 @@
+(define-package "haskell-mode" "20150823.2218" "A Haskell editing mode"
+  '((cl-lib "0.5")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
