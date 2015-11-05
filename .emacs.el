cafb5dd7624945db22dbe777130b229758f8e720
Kevin Esler <kevin.esler@gmail.com>
2015-11-05 16:17:30 -0500

    Updated

--- .emacs.el
+++ .emacs.el
@@ -20,6 +20,8 @@
       (require 'package)
       (add-to-list 'package-archives
                    '("melpa" . "http://melpa.milkbox.net/packages/") t)
+      (add-to-list 'package-archives
+                   '("marmalade" . "https://marmalade-repo.org/packages/") t)
       (package-initialize)))
 
 ;;}}}
--- elpa/ghc-20150830.2213/ghc-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "ghc" "20150830.2213" "Sub mode for Haskell mode"
-  '((haskell-mode "13.0")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
