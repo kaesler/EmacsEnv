67ffd94c6a740b9d8e842abad76de9cde7fa1f24
Kevin Esler <kevin.esler@gmail.com>
2015-12-08 14:11:11 -0500

    Updated

--- elpa/pivotal-tracker-20150901.1221/pivotal-tracker-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "pivotal-tracker" "20150901.1221" "Interact with Pivotal Tracker through its API" 'nil :url "http://github.com/jxa/pivotal-tracker")
--- elpa/shm-20150526.124/shm-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "shm" "20150526.124" "Structured Haskell Mode" 'nil :keywords
-  '("development" "haskell" "structured"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
