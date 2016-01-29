eb9b5c392b1fa534fc26d03f63d0cc6caea36e15
Kevin Esler <kesler@IMPRIVATA.com>
2016-01-29 10:45:54 -0500

    Updated.

--- elpa/ghc-20151220.255/ghc-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "ghc" "20151220.255" "Sub mode for Haskell mode"
-  '((haskell-mode "13.0")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/http-20150716.759/http-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "http" "20150716.759" "Yet another HTTP client" '((emacs "24") (cl-lib "0.5") (request "0.2.0")) :url "https://github.com/emacs-pe/http.el" :keywords '("convenience"))
