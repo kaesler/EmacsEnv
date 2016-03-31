723e5e00dc4d2c4e0dd3172807ef6339e8bd237d
Kevin Esler <kesler@IMPRIVATA.com>
2016-03-31 11:56:29 -0400

    Updated 9 packages.

---
+++ elpa/docker-20160329.1221/docker-pkg.el
@@ -0,0 +1,10 @@
+(define-package "docker" "20160329.1221" "Emacs interface to Docker"
+  '((emacs "24.4")
+    (dash "1.5.0")
+    (magit-popup "2.0.50")
+    (s "1.9.0")
+    (tle "0.1.0"))
+  :url "https://github.com/Silex/docker.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
