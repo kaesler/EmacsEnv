507a6cc2bbd160c3cfb6e9a0835a0555b403a502
Kevin Esler <kesler@IMPRIVATA.com>
2016-04-05 10:22:05 -0400

    Updated various

---
+++ elpa/tle-20160403.532/tle-pkg.el
@@ -0,0 +1,1 @@
+(define-package "tle" "20160403.532" "Tabulated List Extensions" '((dash "1.5.0")) :url "https://github.com/Silex/tabulated-list-extensions" :keywords '("tabulated-list" "extension"))
