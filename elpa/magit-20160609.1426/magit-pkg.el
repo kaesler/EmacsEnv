9fa0a4647702217de12e9db983ce08620160a6e6
Kevin Esler <kesler@IMPRIVATA.com>
2016-06-09 20:31:18 -0400

    Updated

--- elpa/magit-20160609.1426/magit-pkg.el
+++ elpa/magit-20160609.1426/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20160606.1254" "A Git porcelain inside Emacs"
+(define-package "magit" "20160609.1426" "A Git porcelain inside Emacs"
   '((emacs "24.4")
     (async "20150909.2257")
     (dash "20151021.113")
