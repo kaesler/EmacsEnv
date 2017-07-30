c7ba486dc0e1a6a98a8e3d88c098359fa4976704
Kevin Esler <kevin.esler@gmail.com>
2017-07-30 12:52:23 -0400

    Updated Ensime

--- elpa/intero-20170721.1851/intero-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "intero" "20170721.1851" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "aec455424105ddcf8e311acaf880078f7a6d92b3" :url "https://github.com/commercialhaskell/intero" :keywords '("haskell" "tools"))
