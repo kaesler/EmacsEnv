1024e52777df14bcdafdfa9962b4e71957f12218
Kevin Esler <kevin.esler@banno.com>
2017-08-24 10:12:29 -0400

    Updated

--- elpa/bash-completion-20170812.623/bash-completion-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "bash-completion" "20170812.623" "BASH completion for the shell buffer" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/bash-completion-20170813.624/bash-completion-pkg.el
@@ -0,0 +1,4 @@
+(define-package "bash-completion" "20170813.624" "BASH completion for the shell buffer" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
