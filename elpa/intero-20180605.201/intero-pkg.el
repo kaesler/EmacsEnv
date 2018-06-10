6a9faef7ef93c85f791c926da9bf0f4572232d35
Kevin Esler <kevin.esler@gmail.com>
2018-06-10 13:37:38 -0400

    Updated

---
+++ elpa/intero-20180605.201/intero-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "intero" "20180605.201" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "323556bfd65fc8cd817340cbf971a3b46c9e4f17" :keywords '("haskell" "tools") :url "https://github.com/commercialhaskell/intero")
