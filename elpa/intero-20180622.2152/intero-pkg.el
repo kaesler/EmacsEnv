0aeaac84c90ffdd4b2bac37ece03265c3a1cfafb
Kevin Esler <kevin.esler@gmail.com>
2018-07-02 09:58:29 -0400

    Updated,

---
+++ elpa/intero-20180622.2152/intero-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "intero" "20180622.2152" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")))
