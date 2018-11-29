bb603f8f87464084bc0d34df9c9109cb38c80126
kesler <kevin.esler@bamtechmedia.com>
2018-11-29 11:15:02 -0500

    Updated packages.

---
+++ elpa/ibuffer-projectile-20181128.38/ibuffer-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ibuffer-projectile" "20181128.38" "Group ibuffer's list by projectile root" '((projectile "0.11.0")) :commit "013ada3bce9054a3b88c5d9e51073f44d09169d8" :keywords '("themes") :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "http://github.com/purcell/ibuffer-projectile")
