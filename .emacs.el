e4ea91e144346dfd95464aabccb3ae05988f8cab
kesler <kevin.esler@bamtechmedia.com>
2019-04-04 16:26:49 -0400

    Inastalled git-link and js2-mode,

--- .emacs.el
+++ .emacs.el
@@ -1615,14 +1615,12 @@ for common operations.
 (require 'use-package)
 
 ;;{{{ Javascript
-;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-;; (require 'js-comint)
-
-;; (add-hook 'js2-mode-hook
-;;           (lambda ()
-;;             (local-set-key (kbd "C-x C-e") 'js-send-last-sexp)
-;;             (local-set-key (kbd "C-c b") 'js-send-buffer)))
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-x C-e") 'js-send-last-sexp)
+            (local-set-key (kbd "C-c b") 'js-send-buffer)))
 ;;}}}
 ;;{{{ Kaesar
 
