391892fda7849eee8ce626406c2f01dc0788f8df
kesler <kevin.esler@bamtechmedia.com>
2019-04-04 16:21:49 -0400

    Updated.

--- elpa/intero-20181224.1911/intero-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "intero" "20181224.1911" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "382bc27f28101f6ac98dd7f15ca80e7316831bf1" :keywords '("haskell" "tools") :authors '(("Chris Done" . "chrisdone@fpcomplete.com")) :maintainer '("Chris Done" . "chrisdone@fpcomplete.com") :url "https://github.com/commercialhaskell/intero")
---
+++ elpa/intero-20190308.1634/intero-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "intero" "20190308.1634" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "8da81244783fbf03afb49660423c875f2e874fba" :keywords '("haskell" "tools") :authors '(("Chris Done" . "chrisdone@fpcomplete.com")) :maintainer '("Chris Done" . "chrisdone@fpcomplete.com") :url "https://github.com/commercialhaskell/intero")
