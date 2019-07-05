4b977920e8067e9f0d6a92dcc65a3510bf22a070
kesler <kevin.esler@bamtechmedia.com>
2019-07-05 09:29:22 -0400

    Updated many packages.

--- elpa/bash-completion-20180519.1620/bash-completion-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "bash-completion" "20180519.1620" "BASH completion for the shell buffer" 'nil :commit "fbdc78b8770833752ac4bda28e2c3ea764bf8e76" :authors '(("Stephane Zermatten" . "szermatt@gmx.net")) :maintainer '("Stephane Zermatten" . "szermatt@gmx.net"))
--- transient/history.el
+++ transient/history.el
@@ -1,2 +1,6 @@
 ((magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat")
+  (("--" "Jenkins/configure-download-restrictions-ssm.groovy")
+   "--no-ext-diff" "--stat"))
  (magit-push nil))
