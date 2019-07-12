11c42ce611c3887c3dc15bd25b2d6bcc06fcf40f
kesler <kevin.esler@bamtechmedia.com>
2019-07-12 15:32:12 -0400

    Updated.

--- transient/history.el
+++ transient/history.el
@@ -1,5 +1,6 @@
 ((magit-commit nil)
  (magit-diff
+  (("--" "templates/download-domain-events-elasticsearch.yaml"))
   ("--no-ext-diff" "--stat")
   (("--" "Jenkins/configure-download-restrictions-ssm.groovy")
    "--no-ext-diff" "--stat"))
