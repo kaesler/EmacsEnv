5aa8a7a3bfbf582cfdfa826b06306e4c51d4d981
kesler <kevin.esler@bamtechmedia.com>
2019-08-12 14:14:59 -0400

    Updated 16

---
+++ elpa/treemacs-icons-dired-20190719.815/treemacs-icons-dired-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-icons-dired" "20190719.815" "Treemacs icons for dired" '((treemacs "0.0") (emacs "25.2") (cl-lib "0.5")) :commit "d06e2d3f3b3ce77639c0a085d1f3b0e620b1a936" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
---
+++ elpa/treemacs-magit-20190731.540/treemacs-magit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-magit" "20190731.540" "Magit integration for treemacs" '((emacs "25.2") (treemacs "0.0") (pfuture "1.3") (magit "2.90.0")) :commit "d06e2d3f3b3ce77639c0a085d1f3b0e620b1a936" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
