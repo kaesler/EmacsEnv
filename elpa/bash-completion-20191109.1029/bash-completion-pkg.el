88cf67889e5805744b33c1ce2dc494b6271c1190
Kevin Esler <kevin.esler@gmail.com>
2019-11-17 16:29:57 -0500

    Upate packages

---
+++ elpa/bash-completion-20191109.1029/bash-completion-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bash-completion" "20191109.1029" "BASH completion for the shell buffer" 'nil :commit "ac3bd404e87b097eefbf04a0b28676efa4ae13b7" :authors '(("Stephane Zermatten" . "szermatt@gmx.net")) :maintainer '("Stephane Zermatten" . "szermatt@gmx.net"))
