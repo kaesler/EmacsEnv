94f16c99e514a35352d8a3ab731fc7ef60a9c357
Kevin Esler <kevin.esler@gmail.com>
2020-05-02 12:15:49 -0400

    Removed transients.

--- transient/history.el
+++
@@ -1,9 +0,0 @@
-((magit-blame
-  ("-w"))
- (magit-commit nil)
- (magit-diff
-  ("--no-ext-diff" "--stat")
-  (("--" "templates/download-domain-events-elasticsearch.yaml"))
-  (("--" "Jenkins/configure-download-restrictions-ssm.groovy")
-   "--no-ext-diff" "--stat"))
- (magit-push nil))
