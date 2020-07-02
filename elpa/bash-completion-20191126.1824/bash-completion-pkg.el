612e0b20bf5fc6f09e01d73fae690076afaca7e0
kesler <kevin.esler@bamtechmedia.com>
2020-07-02 13:42:05 -0400

    Updated packages.

--- elpa/bash-completion-20191126.1824/bash-completion-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "bash-completion" "20191126.1824" "BASH completion for the shell buffer" 'nil :commit "96ce14af9674f3e605bacca87abc0c23b8f13cd5" :authors '(("Stephane Zermatten" . "szermatt@gmx.net")) :maintainer '("Stephane Zermatten" . "szermatt@gmx.net"))
--- elpa/git-commit-20200516.2016/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20200516.2016" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (transient "20190812") (with-editor "20181103")) :commit "ae82fcf4d47559688a0ad7dfee55dd74bbe14b9e" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
