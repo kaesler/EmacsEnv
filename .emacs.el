05fda822193cc1cfb99a4fe31f9dc5bd208da4be
kesler <kevin.esler@bamtechmedia.com>
2020-12-11 11:02:14 -0500

    Tide mode

--- .emacs.el
+++ .emacs.el
@@ -1627,6 +1627,31 @@ for common operations.
 ;;{{{  Configure MODES and packages.
 
 (require 'use-package)
+;;{{{ Tide for Typescript
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook 'setup-tide-mode)
+(setq tide-format-options
+      '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil)
+)
+
+;;}}}
 ;;{{{ Ido
 ;;(setq ido-enable-flex-matching t)
 ;;(setq ido-everywhere t)
