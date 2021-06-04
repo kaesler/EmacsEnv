aa706ee4bb2a6b2def825216f613104c3018f791
Kevin Esler <kevin.esler@gmail.com>
2021-06-04 13:13:03 +1000

    Updated 21 packages

---
+++ elpa/ibuffer-sidebar-20210508.836/ibuffer-sidebar-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ibuffer-sidebar.el  -*- no-byte-compile: t -*-
+(define-package "ibuffer-sidebar" "20210508.836" "Sidebar for `ibuffer'" '((emacs "25.1")) :commit "fb685e1e43db979e25713081d8ae4073453bbd5e" :authors '(("James Nguyen" . "james@jojojames.com")) :maintainer '("James Nguyen" . "james@jojojames.com") :keywords '("ibuffer" "files" "tools") :url "https://github.com/jojojames/ibuffer-sidebar")
---
+++ elpa/nav-flash-20210510.2035/nav-flash-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from nav-flash.el  -*- no-byte-compile: t -*-
+(define-package "nav-flash" "20210510.2035" "Briefly highlight the current line" 'nil :commit "d76314802273153c2c38156250d75f95ca352482" :authors '(("Roland Walker" . "walker@pobox.com")) :maintainer '("Roland Walker" . "walker@pobox.com") :keywords '("extensions" "navigation" "interface") :url "http://github.com/rolandwalker/nav-flash")
