b87039dab9b88e87f11300d1dfae77bf82c3639b
Kevin Esler <kevin.esler@gmail.com>
2022-07-13 11:45:37 -0400

    Updates.

---
+++ elpa/ormolu-20220530.921/ormolu-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ormolu.el  -*- no-byte-compile: t -*-
+(define-package "ormolu" "20220530.921" "Format Haskell source code using the \"ormolu\" program" '((emacs "24") (reformatter "0.4")) :commit "a6b1d3f8838d067ac5352fb0673c3c3dae7abd73" :authors '(("Vasiliy Yorkin" . "vasiliy.yorkin@gmail.com")) :maintainer '("Vasiliy Yorkin") :keywords '("files" "tools") :url "https://github.com/vyorkin/ormolu.el")
