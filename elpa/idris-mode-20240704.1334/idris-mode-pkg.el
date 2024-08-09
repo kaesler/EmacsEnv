5dfef305ec6ed57aa5e17c74b54a01acac66c6b3
Kevin Esler <kevin.esler@gmail.com>
2024-08-09 09:36:36 -0400

    Update libraries

---
+++ elpa/idris-mode-20240704.1334/idris-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "idris-mode" "20240704.1334" "Major mode for editing Idris code"
+  '((emacs "24")
+    (prop-menu "0.1")
+    (cl-lib "0.5"))
+  :commit "09de86a8f056c61de72c678386039894779a9375" :keywords
+  '("languages")
+  :url "https://github.com/idris-hackers/idris-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/smartrep-20240416.2148/smartrep-pkg.el
@@ -0,0 +1,8 @@
+(define-package "smartrep" "20240416.2148" "Support sequential operation which omitted prefix keys." 'nil :commit "fdf135e3781b286174b5de4d613f12c318d2023c" :authors
+  '(("myuhe" . "yuhei.maeda_at_gmail.com"))
+  :keywords
+  '("convenience")
+  :url "https://github.com/myuhe/smartrep.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
